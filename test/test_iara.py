b826abf440c5f25695f1c5eb2fb143dc26c38a59
carolmartinsfreire <vitoria.freire@lps.ufrj.br>
2026-02-08 23:46:04 -0300

    Add test_iara with metrics selection

--- test/test_iara.py
+++ test/test_iara.py
@@ -1,10 +1,18 @@
 """
 IARA
 """
+# Consideracoes
+#É um script de experimento controlado usado para validar o dataset
+# IARA; validar seletores; validar o pipeline completo (processamento → CV → treino → avaliação)
+#- roda, treina, mede para ver se faz sentido
+#
+
 import argparse
 
 import torch
 import torch.utils.data as torch_data
+import typing
+
 
 import lightning
 import lightning.pytorch.loggers as lightning_log
@@ -12,38 +20,67 @@ import lightning.pytorch.callbacks as lightning_call
 
 import lps_utils.quantities as lps_qty
 
-import lps_ml.utils.device as ml_device
+#import lps_ml.utils.device as ml_device
 import lps_ml.model as ml_model
 import lps_ml.core.cv as ml_cv
 import lps_ml.audio_processors as ml_procs
 import lps_ml.datasets as ml_db
 import lps_ml.utils.general as ml_utils
 import lps_ml.datasets.iara as ml_iara
+# Teste rapidamente quais funções estão disponíveis:
+import lps_sp.signal as lps_signal
+#print(dir(lps_signal))  # Veja todas as funções disponíveis
+from torch import nn
 
-def _evaluate_accuracy(model: torch.nn.Module,
-                      dataloader: torch_data.DataLoader):
-    device = ml_device.get_available_device()
+from iara.ml.metrics import Metric, GridCompiler
+
+
+import logging
+
+
+
+
+
+
+
+#Roda inferência; converte saída do modelo em classe;
+
+
+
+@torch.no_grad()
+def evaluate_from_dataloader(
+    model: nn.Module,
+    dataloader: torch_data.DataLoader,
+    metrics: typing.List[Metric],
+    device: torch.device | None = None,
+) -> dict[Metric, float]:
+
+    if device is None:
+        device = next(model.parameters()).device
+
     model.eval()
     model.to(device)
 
-    correct = 0
-    total = 0
+    y_true = []
+    y_pred = []
 
-    with torch.no_grad():
-        for x, y in dataloader:
-            x, y = x.to(device), y.to(device)
-            out = model(x)
+    for x, y in dataloader:
+        x = x.to(device)
+        y = y.to(device)
 
-            if out.ndim == 1:
-                preds = (out > 0.5).long()
-            else:
-                preds = torch.argmax(out, dim=1)
+        out = model(x)
 
-            correct += (preds == y).sum().item()
-            total += y.size(0)
+        if out.ndim == 1:
+            preds = (out > 0.5).long()
+        else:
+            preds = torch.argmax(out, dim=1)
 
-    acc = correct / total
-    return acc
+        y_true.extend(y.cpu().numpy())
+        y_pred.extend(preds.cpu().numpy())
+
+    return Metric.compute_all(metrics, y_true, y_pred)
+
+
 
 def _main():
     """Main function for the dataset info tables."""
@@ -57,7 +94,28 @@ def _main():
                         help="Maximum number of training epochs.")
     parser.add_argument("--lr", type=float, default=1e-3,
                         help="Learning rate.")
+    parser.add_argument("--debug", action="store_true",
+                        help="DEBUGS.")
+    parser.add_argument("--metrics", nargs="+", default=["accuracy", "balanced_accuracy", "macro_f1"],
+                        help=("Metrics to evaluate. Options: "
+                              "accuracy, balanced_accuracy, micro_f1, macro_f1, "
+                              "macro_recall, micro_recall, macro_precision, micro_precision, "
+                              "detection_probability, sp_index, all"
+                             )
+                        )
+
     args = parser.parse_args()
+
+
+
+
+    #Adicao de logging para retirar debugs
+
+    log_level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(
+        level=log_level,
+        format="%(levelname)s:%(name)s:%(message)s"
+    )
 
     torch.set_float32_matmul_precision('medium')
     ml_utils.set_seed()
@@ -67,22 +125,66 @@ def _main():
     overlap=lps_qty.Time.s(0.75)
 
     dm = ml_db.IARA(
+            #o WAV vira tensor, totalmente independente da classe.
+
             file_processor=ml_procs.TimeProcessor(
                     fs_out=fs_out,
                     duration=duration,
                     overlap=overlap,
-                    pipelines=ml_procs.CPADetector(duration, duration * 60)
+                    #pipelines=ml_procs.CPADetector(duration, duration * 60)
+                    pipelines = ml_procs.CPADetector(
+                        duration,
+                        lps_qty.Time.s(duration.value * 60)
+
+
+                    )
+
                 ),
             cv = ml_cv.FiveByTwo(),
+            #DC.A e um subconjunto do IARA
             data_collection = ml_iara.DC.A,
+            #define o problema de classificacao e retorna um selection
             selection=ml_iara.CargoShipClassifier.IDENTIFIED.as_selector(),
-            batch_size=16)
-
+            batch_size=16,
+            data_dir="C:/Users/carol/Documents/Sonat/IARA/src/data/raw/train"
+            )
+    
+    print("OLD")
     print(ml_utils.format_header(60,"Dataset description"))
     print(dm.to_compile_df())
     print(ml_utils.format_header(60))
     print()
     print(ml_utils.format_header(60,"Training"))
+
+
+    #duplicacao do experimento new selection
+    dm = ml_db.IARA(
+            file_processor=ml_procs.TimeProcessor(
+                fs_out=fs_out,
+                duration=duration,
+                overlap=overlap,
+                pipelines=ml_procs.CPADetector(
+                duration,
+                    lps_qty.Time.s(duration.value * 60)
+                )
+            ),
+            cv=ml_cv.FiveByTwo(),
+            data_collection=ml_iara.DC.A,
+            selection=ml_iara.IARA.ship_category_selector("SHIPTYPE"),
+            batch_size=16,
+            data_dir="C:/Users/carol/Documents/Sonat/IARA/src/data/raw/train"
+        )
+
+
+    print("NEW")
+    print(ml_utils.format_header(60,"Dataset description"))
+    print(dm.to_compile_df())
+    print(ml_utils.format_header(60))
+    print()
+    print(ml_utils.format_header(60,"Training"))
+
+
+
 
     model = ml_model.MLP(
         input_shape=dm.get_sample_shape(),
@@ -108,25 +210,103 @@ def _main():
     trainer = lightning.Trainer(
         max_epochs=args.max_epochs,
         accelerator="auto",
+        # reducao de batches para treinamento mais rapido a fim de debugar o codigo
+        #limit_train_batches=2,
+        #limit_val_batches=2,
         devices="auto",
         logger=logger,
         callbacks=[checkpoint_cb, early_stop_cb],
     )
 
+    # treinamento e eval
     trainer.fit(model, dm)
     trainer.test(model, datamodule=dm)
 
-    train_acc = _evaluate_accuracy(model, dm.train_dataloader())
-    val_acc   = _evaluate_accuracy(model, dm.val_dataloader())
-    # test_acc  = _evaluate_accuracy(model, dm.test_dataloader())
+        # Definicao das metricas para argparse
+    METRIC_MAP = {
+    "accuracy": Metric.ACCURACY,
+    "balanced_accuracy": Metric.BALANCED_ACCURACY,
+
+    "micro_f1": Metric.MICRO_F1,
+    "macro_f1": Metric.MACRO_F1,
+
+    "macro_recall": Metric.MACRO_RECALL,
+    "micro_recall": Metric.MICRO_RECALL,
+
+    "macro_precision": Metric.MACRO_PRECISION,
+    "micro_precision": Metric.MICRO_PRECISION,
+
+    "detection_probability": Metric.DETECTION_PROBABILITY,
+    "sp_index": Metric.SP_INDEX,
+    }
+
+
+    if "all" in args.metrics:
+        metrics = list(METRIC_MAP.values())
+    else:
+        metrics = []
+        for m in args.metrics:
+            key = m.lower()
+            if key not in METRIC_MAP:
+                raise ValueError(f"Métrica inválida: {m}")
+            metrics.append(METRIC_MAP[key])
+    
+    
+
+
+    train_metrics = evaluate_from_dataloader(
+        model,
+        dm.train_dataloader(),
+        metrics
+    )
+
+    val_metrics = evaluate_from_dataloader(
+        model,
+        dm.val_dataloader(),
+        metrics
+    )
+
+    print("Metrics enabled:")
+    for m in metrics:
+        print(f" - {m.name}")
+    print()
+
 
     print(ml_utils.format_header(60))
     print()
-    print(ml_utils.format_header(60,"Results"))
-    print(f"Train accuracy:      {train_acc:.4f}")
-    print(f"Validation accuracy: {val_acc:.4f}")
-    # print(f"Test accuracy:       {test_acc:.4f}")
+    print(ml_utils.format_header(60, "Results (Train)"))
+    for m, v in train_metrics.items():
+        print(f"{m.name:25s}: {v:.4f}")
+
+    print()
+    print(ml_utils.format_header(60, "Results (Validation)"))
+    for m, v in val_metrics.items():
+        print(f"{m.name:25s}: {v:.4f}")
+
     print(ml_utils.format_header(60))
+
+
+
+    #train_acc = _evaluate_accuracy(model, dm.train_dataloader())
+    #val_acc   = _evaluate_accuracy(model, dm.val_dataloader())
+    # test_acc  = _evaluate_accuracy(model, dm.test_dataloader())
+
+    #print(ml_utils.format_header(60))
+    #print()
+    #print(f"Train accuracy:      {train_acc:.4f}")
+    #print(f"Validation accuracy: {val_acc:.4f}")
+    # print(f"Test accuracy:       {test_acc:.4f}")
+    #print(ml_utils.format_header(60, "Results (Train)"))
+    #for m, v in train_metrics.items():
+     #   print(f"{m.name:25s}: {v:.4f}")
+
+    #print()
+    #print(ml_utils.format_header(60, "Results (Validation)"))
+    #for m, v in val_metrics.items():
+     #   print(f"{m.name:25s}: {v:.4f}")
+    #print(ml_utils.format_header(60))    
+
 
 if __name__ == "__main__":
     _main()
+
